b2e8e0f4cc46eebd2e24823c5ad90a9dab6ab9c2
SHARON HERNANDEZ <sharon.dp.hernandez@gmail.com>
2025-11-27 08:40:17 -0500

    add

--- DART_SHARON/ejercicio_1/bin/Control_Secuencial/ecuacion.dart
+++ DART_SHARON/ejercicio_1/bin/Control_Secuencial/ecuacion.dart
@@ -1,5 +1,13 @@
 //Calcule el área de un triángulo en función de las longitudes 
 //de sus lados, utilizando la fórmula:√p(p­a)(p­b)(p­c) donde p = (a+b+c) / 2
+import 'dart:math';
+
 void main(){
-  
+  int a = 3;
+  int b = 4;
+  int c = 5;
+
+  double p =(a+b+c)/2;
+  double area = sqrt(p*(p-a)*(p-b)*(p-c));
+  print("El area de este triangula es de $area"); 
 }
---
+++ DART_SHARON/ejercicio_1/bin/Control_Secuencial/ejer9.dart
@@ -0,0 +1,4 @@
+//Calcular el salario neto de un trabajador en función del 
+//número de horas trabajadas, el precio de la hora y considerando 
+//un descuento fijo al sueldo base por concepto de impuestos del 20%.
+
